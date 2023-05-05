e9c2b36c2c2280d2a94086784e3ffc12be67ec76
Juna <46632782+JunaMeinhold@users.noreply.github.com>
2023-05-05 21:42:14 +0200

    removed unnecessary files

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,31 +1,9 @@
 ﻿namespace TestApp
 {
-    using System.Text;
-
     public static partial class Program
     {
         public static void Main()
         {
-            string fileA = File.ReadAllText("Format.cs");
-            string fileB = File.ReadAllText("FormatSilk.cs");
-            string[] linesA = fileA.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
-            string[] linesB = fileB.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
-
-            string namespaceA = "Format.";
-            string namespaceB = "Silk.NET.DXGI.Format.";
-
-            StringBuilder sbConvert = new();
-            for (int i = 0; i < linesA.Length; i++)
-            {
-                sbConvert.AppendLine($"{namespaceA}{linesA[i]} => {namespaceB}{linesB[i]},");
-            }
-            StringBuilder sbConvertBack = new();
-            for (int i = 0; i < linesA.Length; i++)
-            {
-                sbConvertBack.AppendLine($"{namespaceB}{linesB[i]} => {namespaceA}{linesA[i]},");
-            }
-            File.WriteAllText("convert.txt", sbConvert.ToString());
-            File.WriteAllText("convertback.txt", sbConvertBack.ToString());
         }
     }
 }
